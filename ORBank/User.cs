14c5a90451c60762f5273af21030c64956398929
Monstrous Gemini <fedorcuku82@gmail.com>
2018-10-03 16:02:51 +0300

    Wallet

--- ORBank/User.cs
+++ ORBank/User.cs
@@ -8,5 +8,38 @@ namespace ORBank
 {
     class User
     {
+        private Decimal Money;
+        public Decimal Wallet
+        {
+            get { return Money+65; }
+            set { Money = value-65; }
+        }
+
+        private string Login;
+        private string LogIn
+        {
+            get { return Login; }
+            set { Login = value; }
+        }
+
+        private string PassWord;
+        private string Password
+        {
+            get { return PassWord; }
+            set { PassWord = value; }
+        }
+
+        private string Card_Number;
+        public string CardNum
+        {
+            get { return Card_Number; }
+        }
+
+        private string PIN;
+        public string PINcode
+        {
+            get { return PIN; }
+            set { PIN = value; }
+        }
     }
 }
